85afe7b9ad6944574c30dcbe36712d0c0b42c949
NikitaBudancev <budantsev2@gmail.com>
2021-12-25 00:25:11 +0300

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,17 @@
-// Определяем константы Gulp
-// const { src, dest, series, parallel, watch } = require('gulp');
 const gulp = require('gulp');
-
-var sass = require('gulp-sass')(require('sass'));
-
-// function sass() {
-//     return src('src/sass/**/*.sass')     // берём все SASS-файлы 
-//         .pipe(sass())                    // компилируем SASS в CSS 
-//         .pipe(dest('dist/aseets/css/')); // выгружаем результат 
-// }
-
-// exports.sass = sass;
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', () => {
     return gulp.src('src/scss/*.scss')
         .pipe(sass({
             outputStyle: 'compressed'
-        })
-        .on('error', sass.logError))
+        }))
+        .pipe(autoprefixer('last 2 versions'))
+        .on('error', sass.logError)
         .pipe(gulp.dest('dist/css'))
-        
 });
 
-gulp.task('sass:watch', function(){
+gulp.task('sass:watch', function () {
     gulp.watch('src/**/*.scss', gulp.parallel('sass'));
-  });
+});
