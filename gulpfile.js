f8e12a15a21cc0ac01553040c64b5dd24ad6e77c
NikitaBudancev <budantsev2@gmail.com>
2021-12-27 22:54:30 +0300

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,57 @@
-const gulp = require('gulp');
-
 const { src, dest, watch, parallel } = require('gulp');
 
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify-es').default;
+const concat = require('gulp-concat');
+// const imagemin = require('gulp-imagemin');
 
 function scss() {
     return src('src/scss/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(autoprefixer('last 2 version'))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 version'],
+            grid: true
+        }))
         .on('error', sass.logError)
         .pipe(dest('dist/css'))
 }
 
-function scssWatch() {
-    watch(['src/**/*.scss'], parallel(scss));
+function scripts() {
+    return src([
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/owl.carousel/dist/owl.carousel.js',
+        'src/js/app.js'
+    ])
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(dest('dist/js'))
+}
+
+// function images() {
+//     return src('images/**/*')
+//         .pipe(imagemin([
+//             imagemin.gifsicle({ interlaced: true }),
+//             imagemin.mozjpeg({ quality: 75, progressive: true }),
+//             imagemin.optipng({ optimizationLevel: 5 }),
+//             imagemin.svgo({
+//                 plugins: [
+//                     { removeViewBox: true },
+//                     { cleanupIDs: false }
+//                 ]
+//             })
+//         ]))
+//         .pipe(dest('dist/images'))
+// }
+
+function watching() {
+    watch(['src/**/*.scss'], scss);
+    watch(['src/js/**/*.js'], scripts);
 }
 
 exports.scss = scss;
-exports.scssWatch = scssWatch;
+exports.scripts = scripts;
+exports.watching = watching;
+// exports.images = images;
+
+exports.default = parallel(scripts, watching);
