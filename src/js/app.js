cd0218114142a2f2bd1d68521b19e43c311648e7
NikitaBudancev <budantsev2@gmail.com>
2022-02-01 16:07:51 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -141,20 +141,9 @@ $(function () {
     $('.content-scroll').animate({ scrollTop: 0 }, 500);
   });
 
-  function selectText(containerid) {
-    if (document.selection) { // IE
-      var range = document.body.createTextRange();
-      range.moveToElementText(document.getElementById(containerid));
-      range.select();
-    } else if (window.getSelection) {
-      var range = document.createRange();
-      range.selectNode(document.getElementById(containerid));
-      window.getSelection().removeAllRanges();
-      window.getSelection().addRange(range);
-    }
-  }
+
 
   $('.copy__url').on('click', function () {
-    selectText('copy-url');
+    $(this).select()
   })
 })
